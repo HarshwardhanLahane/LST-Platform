05e46a50286dd581eefc6e78dcdf17e2bdf0aa90
HarshwardhanLahane <harshwardhanlahane45.com>
2025-01-05 18:43:28 +0530

    first commit

---
+++ src/address.ts
@@ -0,0 +1,4 @@
+export const PRIVATE_KEY="429SvtDarSUy4cDVCjmCu5wwv16aXy6hiYPEpsARvBBuPWof3RbRoc66kKi9cyYPzWK3e1ZckRNQozuF7Di7CUkL";
+export const PUBLIC_KEY= "CAPw3vprNAQJwfBDESZx4hKvvxfAskGpZXf91ATwbgHA";
+
+export const TOKEN_MINT_ADDRESS= "";
---
+++ src/mintTokens.ts
@@ -0,0 +1,11 @@
+export const mintTokens = async (fromAddress: string, toAddress:string, amount: number) =>{
+    console.log('Minting Tokens');
+}
+
+export const burnTokens = async (fromAddress: string, toAddress:string, amount: number) =>{
+    console.log('Burning tokens');
+}
+
+export const sendNativeTokens = async (fromAddress: string, toAddress: string, amount: number) =>{
+    console.log('Sending native tokens');
+}
